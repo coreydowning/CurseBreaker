f6d16b6082a38c654251d52446fb90ad43fecffe
Corey Downing <downing.c@gmail.com>
2020-10-03 15:27:09 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="CurseBreaker",
+    version="3.10.2",
+    author="AcidWeb",
+    author_email="pawelj@iosphe.re",
+    url="https://github.com/AcidWeb/CurseBreaker",
+    packages=setuptools.find_packages(),
+    python_requires='~=3.8',
+)
